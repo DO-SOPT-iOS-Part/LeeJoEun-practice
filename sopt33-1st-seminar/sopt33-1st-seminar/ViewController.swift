16a9dd4f0ef52c4c94f95d36423a68baaef1b8e5
LeeJoEun-01 <joeun1005@naver.com>
2023-10-07 14:14:53 +0900

    [ADD] 프로젝트 세팅

---
+++ sopt33-1st-seminar/sopt33-1st-seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sopt33-1st-seminar
+//
+//  Created by 이조은 on 2023/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
