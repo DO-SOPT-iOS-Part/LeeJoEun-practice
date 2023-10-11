bdb76888cacb9b0d42b34c7a6188b71f13e8c527
LeeJoEun-01 <joeun1005@naver.com>
2023-10-11 14:01:11 +0900

    1주차 세미나 실습 및 과제

---
+++ sopt33-1st-seminar/sopt33-1st-seminar/GetDataProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  GetDataProtocol.swift
+//  sopt33-1st-seminar
+//
+//  Created by 이조은 on 2023/10/07.
+//
+
+import Foundation
+
+protocol GetDataProtocol {
+    func getLoginData(email: String, password: String)
+}
---
+++ sopt33-1st-seminar/sopt33-1st-seminar/ResultVC.swift
@@ -0,0 +1,45 @@
+//
+//  ResultVC.swift
+//  sopt33-1st-seminar
+//
+//  Created by 이조은 on 2023/10/07.
+//
+
+import UIKit
+
+
+class ResultVC: UIViewController {
+    var loginDataCompletion: (([String]) -> Void)? //스트링 타입의 배열을 매개변수로 전달 할거고, 리턴타입은 void다!
+
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+
+    var email: String = ""
+    var password: String = ""
+    var delegate: GetDataProtocol?
+
+    private func bindText() {
+        self.emailLabel.text = "email : \(email)"
+        self.passwordLabel.text = "password : \(password)"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bindText()
+    }
+    
+
+    @IBAction func backButton(_ sender: Any) {
+        if let navigationController = self.navigationController {
+            navigationController.popViewController(animated: true)
+        } else {
+            self.dismiss(animated: true)
+        }
+        delegate?.getLoginData(email: self.email,
+                               password: self.password)
+
+        guard let loginDataCompletion else { return }
+        loginDataCompletion([email, password])
+    }
+}
